a53bc976599612a2d8ba61cca41720875133296d
JamesJones4301 <jamesjones32.jj@gmail.com>
2025-08-20 22:52:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -115,5 +115,6 @@ app.get("/", (req, res) => {
 });
 
 // ---------------- START SERVER ----------------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
